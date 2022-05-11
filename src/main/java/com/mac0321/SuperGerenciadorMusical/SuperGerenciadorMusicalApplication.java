65e415cb6beb2171568278c06f1ffccab992c89b
Izaque Sena <izaqueseninha@gmail.com>
2022-05-11 19:54:32 -0300

    Projeto Inicializado com Spring initializr

---
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/SuperGerenciadorMusicalApplication.java
@@ -0,0 +1,13 @@
+package com.mac0321.SuperGerenciadorMusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperGerenciadorMusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperGerenciadorMusicalApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/mac0321/SuperGerenciadorMusical/SuperGerenciadorMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac0321.SuperGerenciadorMusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperGerenciadorMusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
