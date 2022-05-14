930058df98140e1fc9ae4cee91a3e5fc352d3b7a
henriquepaes1 <hpaesdesouza@gmail.com>
2022-05-14 10:30:14 -0300

    uri de autorizacao pronta

--- src/main/java/com/mac0321/SuperGerenciadorMusical/resources/ControleDeAutenticação.java
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/resources/ControleDeAutenticação.java
@@ -3,15 +3,18 @@ package com.mac0321.SuperGerenciadorMusical.resources;
 import java.net.URI;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.SpotifyHttpManager;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping(value = "/")
 public class ControleDeAutenticação{
 	
-	private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080");
+	private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/aceito");
 	private String tokenUsuario = ""; // token que será retornado quando o usuário aceitar a autentiação
 	
 	private static final SpotifyApi acessoApi = new SpotifyApi.Builder()
@@ -20,9 +23,19 @@ public class ControleDeAutenticação{
 			.setRedirectUri(redirectUri)
 			.build();
 	
-	
-	@RequestMapping("/users")
+	@GetMapping("/")
+	private static void  autorizacaoUsuario() {
+		final AuthorizationCodeUriRequest requisicaoDePermissao = acessoApi.authorizationCodeUri()
+				.scope("playlist-modify-private")
+				.show_dialog(true)
+				.build();
+		
+		final URI uriReqAutorizacao = requisicaoDePermissao.execute(); //retorna a URI pronta para a requisição
+		
+	}
+		
+	@RequestMapping("/aceito")
 	String hello() {
-		return "Hello, world";
+		return "Requisicao aceita pelo usuário";
 	}
 }
