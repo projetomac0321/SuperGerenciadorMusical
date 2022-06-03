48f4b520f493c8ae287cfcc32696959fb95cc034
henriquepaes1 <hpaesdesouza@gmail.com>
2022-06-03 11:16:30 -0300

    servico de adicionar playlists

--- src/main/java/com/mac0321/SuperGerenciadorMusical/resources/GerenciadorDeServiços.java
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/resources/GerenciadorDeServiços.java
@@ -5,24 +5,44 @@ import java.io.IOException;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.mac0321.SuperGerenciadorMusical.services.CriadorDePlaylist;
+import com.mac0321.SuperGerenciadorMusical.services.PlaylistsDoUsuárioAtual;
+
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping(value = "/playlists")
 public class GerenciadorDeServiços {
-	/* Criar Classes dos Serviços como atributos */
-	/*
-	@GetMapping("/cria")
-	private Playlist autoriza() throws ParseException, SpotifyWebApiException, IOException {
-		return playListCriada;
-	}
-	*/
+	String accessToken;
+	public CriadorDePlaylist criadorDePlaylists;
+	public PlaylistsDoUsuárioAtual listarPlaylists;
 	
+	
+	/*
+	@PostMapping("/inicializa")
+	Pedir para o front enviar o access token e salvar como argumento do gerenciador de serviços
+	criadorDePlaylists = new CriadorDePlaylist(accessToken);
+	listarPlaylists = new PlaylistDoUsuárioAtual(accessToken );
+	*/
+	@PostMapping("/cria")
+	private Playlist criarPlaylist(@RequestParam String nome, 
+								   @RequestParam boolean serColaborativa, 
+								   @RequestParam boolean serPublica) 
+			throws ParseException, SpotifyWebApiException, IOException {
+		
+		Playlist playlistCriada;
+		playlistCriada = this.criadorDePlaylists.criaPlaylist(nome, serColaborativa, serPublica, accessToken);
+		
+		return playlistCriada;
+	}
 	/*
 	@GetMapping("/addmusica")
 	private void adicionaMusica() throws ParseException, SpotifyWebApiException, IOException {
