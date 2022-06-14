8318a562577a718e2cf1d5e1e01ffc94544dfc1a
Sid <miranda.jp2017@gmail.com>
2022-06-14 15:52:30 -0300

    Classe ProcuradorDePlaylist adicionada

---
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/services/ProcuradorDePlaylist.java
@@ -0,0 +1,32 @@
+package com.mac0321.SuperGerenciadorMusical.services;
+
+import java.io.IOException;
+
+import org.apache.hc.core5.http.ParseException;
+
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+import se.michaelthelin.spotify.requests.data.playlists.GetPlaylistRequest;
+
+public class ProcuradorDePlaylist extends ServiçosDoAplicativo {
+
+	private GetPlaylistRequest getPlaylistRequest;
+
+	ProcuradorDePlaylist(String accessToken) {
+		super(accessToken);
+	}
+
+	  public Playlist executaServiço(String playlistID) {
+		  Playlist playlist_desejada = null;
+		  try {
+			  getPlaylistRequest = spotifyApi.getPlaylist(playlistID).build();
+			  playlist_desejada = getPlaylistRequest.execute();
+	
+		      System.out.println("PLaylist obtida com sucesso!");
+		    } catch (NullPointerException| IOException | SpotifyWebApiException | ParseException exception) {
+		      System.out.println("Não foi possível obter a playlist");
+		    }
+		  return playlist_desejada;
+	  }
+
+}
