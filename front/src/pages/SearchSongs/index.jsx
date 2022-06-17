c07856d039470f39191cd6c9f249c6224c1bf197
Vinícius Viana <viniciusviana020@gmail.com>
2022-06-16 22:21:03 -0300

    Ordenação por acousticness

--- front/src/pages/SearchSongs/index.jsx
+++ front/src/pages/SearchSongs/index.jsx
@@ -6,6 +6,7 @@ import { Outlet, NavLink } from 'react-router-dom';
 
 export function SearchSongs(){
     const [searchInput, setSearchInput] = useState("");
+    const [songsIds, setSongsIds] = useState([]);
     
     const [data, setData] = useState([]); 
     const [searching, setSearching] = useState(false);
@@ -21,6 +22,11 @@ export function SearchSongs(){
           {
               axios.get(`http://localhost:8080/buscar-musicas/buscar-por-query?query=${searchInput}&offset=${offset}`).then(res => {
                   setData(res.data);
+                  for(var i = 0; i < 50; i+= 1)
+                  {
+                      songsIds[i] = (res.data[i].id);
+                  };
+                  setSongsIds(songsIds);
                 }).catch(err => console.log(err.message));
               setOffset(offset + 50);
               handleSearch(true);  
@@ -29,8 +35,44 @@ export function SearchSongs(){
           {
             handleSearch(false);
             setOffset(0);
+            setSongsIds([]);
           }
         }
+
+        const [parameters, setParameters] = useState([]);
+
+        const getParameters = (songsIds) => {
+            axios.get(`http://localhost:8080/buscar-musicas/obter-parametros-das-musicas?idsDasMusicas=${songsIds}`)
+            .then(res => {
+                setParameters(res.data);
+            }).catch(err => console.log(err.message));
+               parameters.sort(function ordenaPorParametro(a, b){
+                  if(a.acousticness > b.acousticness)
+                    return -1;
+                  if(a.acousticness < b.acousticness)
+                    return 1;
+                  return 0;
+                })
+                setParameters(parameters);
+               data.sort(function comparaId(a, b){
+                  for(var i = 0; i < 50; i += 1)
+                  {
+                      if(parameters[i].id == a.id)
+                        return -1;
+                      if(parameters[i].id == b.id)
+                        return 1;
+                  }
+                    return 0;  
+               })
+               setSongsIds(data);
+               data.forEach(function(song, index, arr){
+                  parameters.forEach(function(item, indice, array){
+                    if(item.id == song.id)
+                      song.acousticness = item.acousticness
+                  })
+               })
+        }
+
 
     return(
         <div className="home">
@@ -61,6 +103,7 @@ export function SearchSongs(){
                                             <div className="songInfo">
                                                 <div className="songRowText">
                                                     <h1>{result.name}</h1>
+                                                    <p>{result.acousticness}</p>
                                                 </div>
                                             </div>
                                             <div className="plus">
@@ -82,6 +125,7 @@ export function SearchSongs(){
                           </div>
                       </div>
                 </nav>
+                    <button onClick={e =>{e.preventDefault(); getParameters(songsIds)}}></button>
                         <Outlet/>
         </div>
     );
