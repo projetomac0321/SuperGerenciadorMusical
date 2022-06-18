d05647ef15e8fa858078212635eaf1d1e0e5a3b6
Izaque Sena <izaqueseninha@gmail.com>
2022-06-17 22:40:46 -0300

    GerenciadorDaTabelaDaPlaylist

---
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/controllers/GerenciadorDaTabelaDaPlaylist.java
@@ -0,0 +1,32 @@
+package com.mac0321.SuperGerenciadorMusical.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mac0321.SuperGerenciadorMusical.services.Autenticador;
+import com.mac0321.SuperGerenciadorMusical.services.OrdenadorDeMúsicasPorParâmetro;
+import com.mac0321.SuperGerenciadorMusical.services.ProcuradorDeMúsicas;
+
+import se.michaelthelin.spotify.model_objects.specification.Track;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping(value = "/tabela-da-playlist")
+public class GerenciadorDaTabelaDaPlaylist {
+	private OrdenadorDeMúsicasPorParâmetro ordenadorDeMusicasPorParametro;
+	private ProcuradorDeMúsicas procuradorDeMusicas;
+	private Autenticador autenticador = Autenticador.criarAutenticador();
+	
+	@GetMapping("/ordenar-por-paramaetro")
+	private <T extends Comparable<T>> ResponseEntity<Track[]> listarMusicasOrdenadasPorParametro(T[] parametro, String[] ids) {
+		Track[] listaDeMusicasOrdenada;
+		ordenadorDeMusicasPorParametro = new OrdenadorDeMúsicasPorParâmetro();
+		procuradorDeMusicas = new ProcuradorDeMúsicas(autenticador.getTokenUsuario());
+		listaDeMusicasOrdenada = procuradorDeMusicas.executaServiço(ordenadorDeMusicasPorParametro.ordenaMúsicas(parametro, ids));
+		return new ResponseEntity<Track[]>(listaDeMusicasOrdenada, HttpStatus.OK);
+	}
+}
