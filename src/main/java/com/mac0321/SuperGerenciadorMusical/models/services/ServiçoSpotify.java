4dd1f8d9dd19c4ec218183438cd422d79af9090c
Sid <miranda.jp2017@gmail.com>
2022-06-20 16:35:22 -0300

    Merge etapa3 finalizado

--- src/main/java/com/mac0321/SuperGerenciadorMusical/models/services/ServiçoSpotify.java
+++ src/main/java/com/mac0321/SuperGerenciadorMusical/models/services/ServiçoSpotify.java
@@ -2,11 +2,7 @@ package com.mac0321.SuperGerenciadorMusical.models.services;
 
 import se.michaelthelin.spotify.SpotifyApi;
 
-<<<<<<< HEAD
 public class ServiçoSpotify extends ServiçosDoAplicativo {
-=======
-public abstract class ServiçoSpotify extends ServiçosDoAplicativo {
->>>>>>> etapa3
 
 	protected final SpotifyApi spotifyApi;
 	public ServiçoSpotify(String accessToken){
