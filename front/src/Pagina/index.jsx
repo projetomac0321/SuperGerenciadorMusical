4cfe2ee4fdb9996423e6ea8b8e19ffebd2ee32a5
Luiz <luizflaminio@usp.br>
2022-07-04 08:36:41 -0300

    front novo autenticacao

--- front/src/Pagina/index.jsx
+++ front/src/Pagina/index.jsx
@@ -1,73 +1,80 @@
-import React from 'react';
+import {React} from 'react';
 import './styles.css';
 import { Outlet, NavLink } from 'react-router-dom';
-import Logo from '../Imagens/gm.png';
+import Logo from '../images/gm.png';
 import { FiList, FiPlusSquare, 
          FiMusic, FiBook, 
-         FiBookOpen, FiEdit3,
-         FiFilter
+         FiBookOpen, FiEdit3
        } from 'react-icons/fi';
-
-export function Pagina() { 
+export function App() { 
+    
     return (
         <div className="page">
                <div className="sidebar">
-                  <img 
+                <nav className='toMenu'>
+                    <NavLink id='menuLink'
+                             className="link"
+                             to="/home">
+                        <img 
                       className="sidebarLogo"
                       src={Logo} 
                       alt="logo"
                     />
+                    
+                    </NavLink>
                     <hr className="divider"/>
-
+                </nav>
                 <nav className="links">                      
-                    <NavLink className="link"      
+                    <NavLink id="searchSongsLink"
+                             className="link"      
                              to="/searchsongs">
                         <FiMusic className="symbol"/> 
                         Procurar Músicas
                     </NavLink>
 
-                    <NavLink className="link"      
+                    <NavLink id="searchAlbumsLink"
+                             className="link"      
                              to="/searchalbums">
                         <FiBook className="symbol"/> 
                         Procurar Álbuns
                     </NavLink>
 
-                    <NavLink className="link"      
+                    <NavLink id="searchPlaylistsLink"
+                             className="link"      
                              to="/searchplaylists">
                         <FiBookOpen className="symbol"/> 
                         Procurar Playlists
                     </NavLink>
 
-                    <NavLink className="link"      
+                    <NavLink id="searchArtistsLink"
+                             className="link"      
                              to="/searchartists">
                         <FiEdit3 className="symbol"/> 
                         Procurar Artistas
                     </NavLink>
 
-                    <NavLink className="link" 
+                    <NavLink id="listLink"
+                             className="link" 
                              to="/listplaylists">
                         <FiList className="symbol"/> 
                         Listar Playlists
                     </NavLink>
  
-                    <NavLink className="link" 
+                    <NavLink id="createLink"
+                             className="link" 
                              to="/createplaylists">
                         <FiPlusSquare className="symbol"/>   
                         Criar Playlists
                     </NavLink>
 
-                    <NavLink className="link" 
-                             to="/buscar-nas-playlists">
-                        <FiFilter className="symbol"/>   
-                        Filtragem nas Playlists
-                    </NavLink>
-
                  </nav>
+            
             </div>
                 
             <div className="body">
-                <Outlet />
+                    <Outlet />
             </div>
         </div>
   )
 }
+
